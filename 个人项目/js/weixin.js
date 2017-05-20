e9b6418e4fcee466a144bc138affbcee2cbb84a3
niufengjun <1287774147@qq.com>
2017-05-20 18:39:12 +0800

    基本完成
    动画效果，以及一些内容添加

--- 个人项目/js/weixin.js
+++ 个人项目/js/weixin.js
@@ -1,5 +1,4 @@
 $(function(){
-	console.log($(".nav_top .nav ul li"));
 	$(".nav_top .nav ul li").hover(function(){
 		$(this).css({
 			"backgroundColor":"orange",
@@ -15,5 +14,18 @@ $(function(){
 			"color":"red"
 		});
 	});
-	
+	$(".nav_top .nav ul li").click(function(){
+		location.reload(true); 
+	})
+	$(".nav_top .nav ol li").click(function(){
+		location.reload(true); 
+	})
+
+	$("#function .function #action ul li img").hover(function(){   
+        var a=$(this).attr("src").substring(0,12);
+        $(this).attr("src",a+"_1.png");
+	},function(){
+		var a=$(this).attr("src").substring(0,12);
+        $(this).attr("src",a+".png");
+	})
 })
